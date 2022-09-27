6ae5e9af0454ff46e5dfa6bffeb523828336f18e
FreddyCoello <freddyantonio155@gmail.com>
2022-09-27 18:56:19 -0300

    27-09-2022

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const alumnosRouter = require('./src/routes/alumnos')
+const db = require('./db')
+
+app.set('view engine', 'ejs')
+app.set('views', __dirname + '/src/views')
+
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+
+app.use(express.static('public'))
+app.set('views', __dirname + '/src/views')
+
+const alumnos = require('./src/routes/alumnos')
+app.use(alumnosRouter)
+
+app.listen(3020, ()=>{
+    console.log('¡Server UP! en http://localhost:3020')
+})
